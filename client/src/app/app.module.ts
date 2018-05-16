6feb22e1ecd8a9a18324be50723efa26dff7f9ed
NashPL <nash25pl@gmail.com>
2018-05-16 23:59:48 +0100

    changelog: 	-> finnaly decided on a design schema 		* Bootstrap for a layout and some components 		* Material Angluar io for interactive component and grid 	-> created a modal for sigin and nav bar 	-> update on npm modules

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,14 +1,42 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+import { MatButtonModule, MatCheckboxModule, MatIconModule, MatToolbarModule, MatSidenavModule, MatGridListModule, MatFormFieldModule, MatInputModule, MatDialogModule } from '@angular/material';
+
+
+
+
+
+import {
+} from '@angular/material';
+import { NavComponent } from './nav/nav.component';
+import { SigninComponent } from './signin/signin.component';
+
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    SigninComponent
+  ],
+  entryComponents: [
+    SigninComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatCheckboxModule,
+    MatButtonModule,
+    MatIconModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatDialogModule,
+    MatGridListModule,
+    MatFormFieldModule,
+    MatInputModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
