d5e4cc0f7989edb8b3a75c9cf7a50ee7b390cb35
NashPL <nash25pl@gmail.com>
2018-05-23 21:32:26 +0100

    kk

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,6 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
 
 import { AppComponent } from './app.component';
 
@@ -28,6 +31,8 @@ import { SigninComponent } from './signin/signin.component';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    FormsModule,
+    HttpClientModule,
     MatCheckboxModule,
     MatButtonModule,
     MatIconModule,
--- client/src/app/signin/signin.component.ts
+++ client/src/app/signin/signin.component.ts
@@ -1,16 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
 import { Inject } from '@angular/core';
+import { USER } from './../models/-user';
+
 @Component({
   selector: 'app-signin',
   templateUrl: './signin.component.html',
   styleUrls: ['./signin.component.css']
 })
 export class SigninComponent implements OnInit {
-
   ngOnInit() {
   }
-
 
   constructor(
     public dialogRef: MatDialogRef<SigninComponent>,
@@ -18,5 +18,9 @@ export class SigninComponent implements OnInit {
 
   onNoClick(): void {
     this.dialogRef.close();
+  }
+
+  loginSubmit(postValues): void {
+
   }
 }
